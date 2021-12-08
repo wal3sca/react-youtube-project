99b189b3ea6ff97f1d70e71588f058749a57002c
Taylor W <taylorwilliams@pursuit.org>
2021-12-08 12:23:59 -0500

    Updated NavBar.js with Routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const NavBar = () => {
     return (
         <header className='nav'>
             <span id='youtube'>Youtube</span>
-            Home 
-            About
+
+            <Link to='/'>Home</Link>
+            <Link to='about'>About</Link>
+
         </header>
     )
 }
