9fca2f4dbdf8d4e4d3c1d9c4200461fb5a5593a4
Eva Rodriguez <evarodriguez@Evas-MBP.lan>
2021-12-08 12:22:38 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
 import SearchBar from './Home/SearchBar';
 import Videos from './Videos/VideosRender'
+import React from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
+import Comments from "./components/Comments";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       <SearchBar/>
       <Videos/>
+      <Home />
+      <About />
+      <NavBar />
+      <Comments />
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
