bccc4e7a72fbc4020e9e0bb21fc68adcbe4b9b0f
Taylor W <taylorwilliams@pursuit.org>
2021-12-09 13:58:13 -0500

    App.js updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
 
-import SearchBar from './Home/SearchBar';
-import Videos from './Videos/VideosRender'
+// import SearchBar from './Home/SearchBar';
+// import Videos from './Videos/VideosRender'
 import React from "react";
-import Home from "./components/Home";
-import About from "./components/About";
+// import Home from "./components/Home";
+// import About from "./components/About";
 import NavBar from "./components/NavBar";
-import Comments from "./components/Comments";
+// import Comments from "./components/Comments";
 
 
 export default function App() {
   return (
 
     <div>
-      <Home />
-      <About />
+      {/* <Home />
+      <About /> */}
       <NavBar />
-      <SearchBar />
+      {/* <SearchBar />
       <Videos/>
-      <Comments />
+      <Comments /> */}
     </div>
   );
 }
 
-export default App;
+
