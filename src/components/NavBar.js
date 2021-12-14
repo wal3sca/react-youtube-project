92b2b4129839304b3c64d80da2df731291997bd6
Taylor W <taylorwilliams@pursuit.org>
2021-12-13 22:12:33 -0500

    Added CSS for the NavBar Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,18 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import navbar from '../Videos/style/navbar.css';
 
 const NavBar = () => {
     return (
         <header className='nav'>
+            <strong id='youtube'>YouTube</strong>
+
+            <strong id='home'>
             <Link to='/Home'>Home</Link>
+            </strong>
+
+            <strong id='about'>
             <Link to='/about'>About</Link>
+            </strong>
 
         </header>
     )
